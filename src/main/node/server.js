680c667b06b88b5014dcea506545613075973b50
Eric Bottard <eric.bottard+github@gmail.com>
2017-11-24 19:38:20 +0100

    Use content negociation
    Fixes #2

--- src/main/node/server.js
+++ src/main/node/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Negotiator = require('negotiator');
 
 const PORT = 8080;
+const SUPPORTED_MEDIA_TYPES = ['text/plain', 'application/json' /*TODO add more*/]; // In preference order
 
 const app = express();
 
@@ -15,8 +17,21 @@ var fn = require(process.env.FUNCTION_URI);
 app.post('/', function (req, res) {
     var resultx = fn(req.body);
     console.log("Result " + resultx);
-    res.type("text/plain");
-    res.status(200).send(resultx);
+
+    negotiator = new Negotiator(req);
+
+    switch (negotiator.mediaType(SUPPORTED_MEDIA_TYPES)) { // returns the most preferred Accept'ed type intersected with our list
+        case 'application/json':
+            res.type("application/json").json(resultx);
+            break;
+        case 'text/plain':
+            // Force text/plain before calling send, as it defaults to html for strings
+            res.type("text/plain").send("" + resultx);
+            break;
+
+    }
+
+    res.status(200);
 });
 
 app.listen(PORT);

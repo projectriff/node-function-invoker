cef036354e579f59be2757c1b04c39bdbef45393
Scott Andrews <sandrews@pivotal.io>
2018-04-12 13:03:14 -0400

    typo

--- samples/mean/mean.js
+++ samples/mean/mean.js
@@ -4,10 +4,10 @@ const miss = require('mississippi');
 module.exports = (input, output) => {
     let n = 0;
     let mean = 0;
-    const meanSteam = miss.through.obj((newValue, _, callback) => {
+    const meanStream = miss.through.obj((newValue, _, callback) => {
         mean = stats.addToMean(mean, n++, newValue);
         callback(null, mean);
     });
-    input.pipe(meanSteam).pipe(output);
+    input.pipe(meanStream).pipe(output);
 };
 module.exports.$interactionModel = 'node-streams';

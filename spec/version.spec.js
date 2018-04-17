5a5bdc3ce7f2ddcb904843f833b93d5803dace53
Scott Andrews <sandrews@pivotal.io>
2018-04-17 14:04:53 -0400

    Align versions

--- spec/version.spec.js
+++ spec/version.spec.js
@@ -18,7 +18,7 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-fdescribe('version', () => {
+describe('version', () => {
 
     const { version } = require('../package.json');
 

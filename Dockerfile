57dd4d88905c415f8d6e7d9c6d1733a01947a832
Scott Andrews <sandrews@pivotal.io>
2018-11-01 14:19:49 -0400

    Drop invoker build support
    Function builds going forward will use buildpacks. We no longer need to
    build or publish docker images from this repo.
    
    Fixes #71

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:10.7.0-alpine
-WORKDIR /usr/app
-COPY . .
-ENV HOST 0.0.0.0
-ENV GRPC_PORT 10382
-ENV HTTP_PORT 8080
-RUN npm install --production
-EXPOSE 10382 8080
-CMD ["node", "server.js"]
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-version=${1:-`cat package.json | jq -r '.version'`}
-
-docker build . -t "projectriff/node-function-invoker:latest"
-docker tag "projectriff/node-function-invoker:latest" "projectriff/node-function-invoker:${version}"

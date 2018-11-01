f50aa452bdbe663d11a07fe92cd507414f19e70c
Scott Andrews <sandrews@pivotal.io>
2018-11-01 19:20:00 -0400

    polish

---
+++ .travis/publish-gcs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=${1:-`cat package.json | jq -r '.version'`}
+commit=$(git rev-parse HEAD)
+
+gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
+
+npm pack
+
+package=projectriff-node-function-invoker-${version}.tgz
+bucket=gs://projectriff/node-function-invoker/releases
+
+gsutil cp -a public-read ${package} ${bucket}/v${version}/node-function-invoker-${version}.tgz
+gsutil cp -a public-read ${package} ${bucket}/v${version}/snapshots/node-function-invoker-${version}-${commit}.tgz
+gsutil cp -a public-read ${package} ${bucket}/latest/node-function-invoker.tgz

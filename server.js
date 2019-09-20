086da39d63accc394bb92eb2e9bf888bec5e56df
Florent Biville <fbiville@pivotal.io>
2019-09-20 17:09:54 +0200

    [#95] Rename port envvar

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const userFunctionUri = process.env.FUNCTION_URI;
 if (typeof userFunctionUri === 'undefined' || userFunctionUri.trim() === '') {
     throw 'FUNCTION_URI envvar not set or empty. Aborting.'
 }
-const port = process.env.PORT || '8081';
+const port = process.env.GRPC_PORT || '8081';
 
 startInvoker(userFunctionUri, port);

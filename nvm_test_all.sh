56835168447c4ac6be94dfb47d26dae33e30d059
Florent Biville <fbiville@pivotal.io>
2020-03-25 17:59:18 +0100

    Do not fail on first failure

--- nvm_test_all.sh
+++ nvm_test_all.sh
@@ -6,10 +6,15 @@ NVM_SOURCE=${NVM:-/usr/local/opt/nvm/nvm.sh}
 # shellcheck source=/dev/null
 source "${NVM_SOURCE}"
 
+failed=false
 supported_node_versions=(10 12 13)
 for node_version in "${supported_node_versions[@]}"; do
 	nvm use "${node_version}" \
 	&& npm --silent rebuild 1>/dev/null \
 	&& npm --silent install \
-	&& npm --silent test
+	&& npm --silent test || failed=true;
 done
+
+if "$failed" = true; then
+  exit 42
+fi

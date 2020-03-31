e8ff9ba2830e75aa9ff2a114b198589013a50e8a
Florent Biville <fbiville@pivotal.io>
2020-03-31 14:18:43 +0200

    Handle request-reply function error
    Fixes #133

--- spec/request-reply-promoter.errors.spec.js
+++ spec/request-reply-promoter.errors.spec.js
@@ -1,4 +1,5 @@
 const promoteFunction = require("../lib/request-reply-promoter");
+const { PassThrough } = require("stream");
 
 describe("function promoter =>", () => {
     describe("when called with functions with an argument transformer => ", () => {
@@ -26,6 +27,36 @@ describe("function promoter =>", () => {
                     "Request-reply function must declare exactly 1 argument transformer. Found 2"
                 );
             }
+        });
+    });
+
+    describe("when the called function throws => ", () => {
+        const userFunction = (x) => {
+            throw new Error("nope");
+        };
+
+        it("propagates the error back to the input stream", (done) => {
+            const streamingUserFunction = promoteFunction(userFunction);
+            const input = new PassThrough({ objectMode: true });
+            const output = new PassThrough({ objectMode: true });
+
+            input.on("error", (err) => {
+                expect(err.type).toEqual(
+                    "request-reply-function-runtime-error"
+                );
+                expect(err.cause).toEqual(new Error("nope"));
+                done();
+            });
+            output.on("error", (err) => {
+                done(
+                    new Error(
+                        `Expected output to not receive any error, got: ${err}`
+                    )
+                );
+            });
+
+            streamingUserFunction({ $order: [input] }, { $order: [output] });
+            input.write("irrelevant value");
         });
     });
 });

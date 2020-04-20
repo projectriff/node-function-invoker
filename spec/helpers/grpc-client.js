30467aefeb5ca63db02b0489b3249a1c0413fe13
Florent Biville <fbiville@pivotal.io>
2020-04-20 17:08:23 +0200

    Fix grpc client definition

--- spec/helpers/grpc-client.js
+++ spec/helpers/grpc-client.js
@@ -2,5 +2,8 @@ const services = require("../../codegen/proto/riff-rpc_grpc_pb");
 const grpc = require("@grpc/grpc-js");
 
 module.exports = (address) => {
-    return new services.RiffClient(address, grpc.credentials.createInsecure());
+    const clientConstructor = grpc.makeGenericClientConstructor(
+        services["streaming.Riff"]
+    );
+    return new clientConstructor(address, grpc.credentials.createInsecure());
 };

ab102e5adffbf7d45d98f51a08736f4bb9f88adc
Scott Andrews <sandrews@pivotal.io>
2020-05-13 10:53:25 -0400

    Drop GCS ACL since the bucket has a uniform access policy now

--- .github/workflows/publish-gcs.sh
+++ .github/workflows/publish-gcs.sh
@@ -6,6 +6,6 @@ commit=$(git rev-parse HEAD)
 package=projectriff-node-function-invoker-${version}.tgz
 bucket=gs://projectriff/node-function-invoker/releases
 
-gsutil cp -a public-read ${package} ${bucket}/v${version}/node-function-invoker-${version}.tgz
-gsutil cp -a public-read ${package} ${bucket}/v${version}/snapshots/node-function-invoker-${version}-${commit}.tgz
-gsutil cp -a public-read ${package} ${bucket}/latest/node-function-invoker.tgz
+gsutil cp ${package} ${bucket}/v${version}/node-function-invoker-${version}.tgz
+gsutil cp ${package} ${bucket}/v${version}/snapshots/node-function-invoker-${version}-${commit}.tgz
+gsutil cp ${package} ${bucket}/latest/node-function-invoker.tgz
